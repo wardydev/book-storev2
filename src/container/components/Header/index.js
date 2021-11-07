8d609d9a5d538da0d0f7d4cab18ed14c4eb1e560
wardy dev <wardydev@gmail.com>
2021-11-06 23:35:44 -0800

    creating hero sections

--- src/container/components/Header/index.js
+++ src/container/components/Header/index.js
@@ -1,9 +1,52 @@
 import React from 'react'
 
+// assets
+import Logo from '../../../assets/logo/logo.svg'
+
+// react router
+import { Link } from 'react-router-dom'
+
 export default function Header() {
     return (
-        <div>
-            header <span className="bg-red-500">components</span>
-        </div>
+        <header className="px-20 py-8">
+            <div className="container-header flex items-center justify-between">
+                <Link to="/">
+                    <img src={Logo} alt="book logo" width="130" />
+                </Link>
+                <div className="border-2 border-purple-200 rounded-lg flex items-center w-3/5">
+                    <div className="flex items-center space-x-2 border-r-2 border-purple-200 px-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-purple-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+                        </svg>
+
+                        <div className="flex items-center ml-2 text-purple-500 cursor-pointer">
+                            <span>Menus</span>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="w-full">
+                        <input type="text" className="focus:outline-none py-2 px-4 w-full" placeholder="Search over 30 million book titles" />
+                    </div>
+                    <div className="px-4 border-l-2 border-purple-200 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-purple-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
+                    </div>
+                </div>
+                <div className="signs flex items-center space-x-6">
+                    <button className="py-2 px-3 bg-purple-100 text-purple-500 rounded-lg hover:bg-purple-200">Log in</button>
+                    <button className="flex items-center justify-center py-2 px-3 bg-purple-500 hover:bg-purple-600 text-gray-50 rounded-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                    </svg>
+
+                    <span>Sign Out</span>
+                    </button>
+                </div>
+            </div>
+        </header>
     )
 }
--- src/container/sections/home/Hero/index.js
+++ src/container/sections/home/Hero/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import './index.css'
 
 export default function Hero() {
     return (
-        <div>
-            hero <span className="bg-blue-500">section</span>
-        </div>
+        <section className="px-20">
+            <div className="container flex space-x-4">
+                <div className="hero-special grid-1 w-3/4 min-h-20 bg-purple-100 rounded-lg py-14 px-12">
+                    <div className="hero-title text-gray-800 w-3/5 pb-8">
+                        <span className="text-purple-500">BACK TO SCHOOL</span>
+                        <h1 className="text-5xl font-bold mt-4">Special 50% Off</h1>
+                        <h3 className="text-3xl mb-4">for our student community</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+
+                        <div className="flex space-x-4 items-center mt-8">
+                            <button className="flex items-center bg-purple-500 hover:bg-purple-600 rounded-lg py-3 px-4 text-gray-50 filter drop-shadow-xl">
+                                <span className="mr-3">Get the deal</span>
+
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                                </svg>
+                            </button>
+                            <button className="py-3 px-4 border-2 border-gray-600 rounded-lg hover:bg-purple-500 hover:border-0 hover:text-gray-50">See Other Promos</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid-1 w-1/4 h-20 bg-red-500 rounded-lg">grid 2</div>
+            </div>
+        </section>
     )
 }
