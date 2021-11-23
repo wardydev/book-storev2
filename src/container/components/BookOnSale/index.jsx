ae466be18e36626dc29eede2336e68eb420febb4
wardy dev <wardydev@gmail.com>
2021-11-22 20:51:25 -0800

    finished book on sale

--- src/container/components/BookOnSale/index.jsx
+++ src/container/components/BookOnSale/index.jsx
@@ -1,9 +1,102 @@
 import React from 'react'
 
+// img
+import BookOne from '../../../assets/images/bookOnSale/img1.jpg'
+
 export default function BookOnSale() {
     return (
-        <div>
-            Book on sale
-        </div>
+        <section className="px-20 py-28">
+            <div className="container">
+                <div className="mb-8">
+                    <h3 className="text-4xl my-2 text-gray-900 font-bold">Book On Sale</h3>
+                </div>
+                <div className="grid grid-cols-6 gap-8">
+                    <div>
+                        {/* img */}
+                        <img src={BookOne} alt="book-1" className="w-full rounded-2xl" />
+                        {/* title */}
+                        <div className="mt-4 mb-4">
+                            <h4 className="text-gray-900 font-medium">The Missadventure of..</h4>
+                            <p className="text-gray-500">ADVANTURE, SURVIVAL</p>
+                        </div>
+                        {/* flex rate and price */}
+                        <div className="flex justify-between items-center">
+                            <span className="text-yellow-500">4.6</span>
+                            <span className="font-medium text-2xl">$45.4</span>
+                        </div>
+                    </div>
+                    <div>
+                        {/* img */}
+                        <img src={BookOne} alt="book-1" className="w-full rounded-2xl" />
+                        {/* title */}
+                        <div className="mt-4 mb-4">
+                            <h4 className="text-gray-900 font-medium">The Missadventure of..</h4>
+                            <p className="text-gray-500">ADVANTURE, SURVIVAL</p>
+                        </div>
+                        {/* flex rate and price */}
+                        <div className="flex justify-between items-center">
+                            <span className="text-yellow-500">4.6</span>
+                            <span className="font-medium text-2xl">$45.4</span>
+                        </div>
+                    </div>
+                    <div>
+                        {/* img */}
+                        <img src={BookOne} alt="book-1" className="w-full rounded-2xl" />
+                        {/* title */}
+                        <div className="mt-4 mb-4">
+                            <h4 className="text-gray-900 font-medium">The Missadventure of..</h4>
+                            <p className="text-gray-500">ADVANTURE, SURVIVAL</p>
+                        </div>
+                        {/* flex rate and price */}
+                        <div className="flex justify-between items-center">
+                            <span className="text-yellow-500">4.6</span>
+                            <span className="font-medium text-2xl">$45.4</span>
+                        </div>
+                    </div>
+                    <div>
+                        {/* img */}
+                        <img src={BookOne} alt="book-1" className="w-full rounded-2xl" />
+                        {/* title */}
+                        <div className="mt-4 mb-4">
+                            <h4 className="text-gray-900 font-medium">The Missadventure of..</h4>
+                            <p className="text-gray-500">ADVANTURE, SURVIVAL</p>
+                        </div>
+                        {/* flex rate and price */}
+                        <div className="flex justify-between items-center">
+                            <span className="text-yellow-500">4.6</span>
+                            <span className="font-medium text-2xl">$45.4</span>
+                        </div>
+                    </div>
+                    <div>
+                        {/* img */}
+                        <img src={BookOne} alt="book-1" className="w-full rounded-2xl" />
+                        {/* title */}
+                        <div className="mt-4 mb-4">
+                            <h4 className="text-gray-900 font-medium">The Missadventure of..</h4>
+                            <p className="text-gray-500">ADVANTURE, SURVIVAL</p>
+                        </div>
+                        {/* flex rate and price */}
+                        <div className="flex justify-between items-center">
+                            <span className="text-yellow-500">4.6</span>
+                            <span className="font-medium text-2xl">$45.4</span>
+                        </div>
+                    </div>
+                    <div>
+                        {/* img */}
+                        <img src={BookOne} alt="book-1" className="w-full rounded-2xl" />
+                        {/* title */}
+                        <div className="mt-4 mb-4">
+                            <h4 className="text-gray-900 font-medium">The Missadventure of..</h4>
+                            <p className="text-gray-500">ADVANTURE, SURVIVAL</p>
+                        </div>
+                        {/* flex rate and price */}
+                        <div className="flex justify-between items-center">
+                            <span className="text-yellow-500">4.6</span>
+                            <span className="font-medium text-2xl">$45.4</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
