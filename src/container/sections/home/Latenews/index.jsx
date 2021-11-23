dc39e6c991b83257b68c4270d14f3c4b88e48d5a
wardy dev <wardydev@gmail.com>
2021-11-23 03:07:46 -0800

    finished latest news

--- src/container/sections/home/Latenews/index.jsx
+++ src/container/sections/home/Latenews/index.jsx
@@ -1,9 +1,86 @@
 import React from 'react'
+// img
+import BookOne from '../../../../assets/images/offers/img3.jpg'
 
 export default function Latenews() {
     return (
-        <div>
-            late news
-        </div>
+        <section className="px-20 py-12">
+            <div className="container">
+                <div className="flex justify-between items-end mb-12">
+                    <div className="w-2/5">
+                        <h3 className="text-4xl my-2 text-gray-900 font-bold">Lates News</h3>
+                        <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse aliquam consequuntur corrupti nam unde! Quaerat</p>
+                    </div>
+                    <button className="bg-purple-500 hover:bg-purple-600 font-medium py-2 px-6 text-gray-50 rounded-lg">View More</button>
+                </div>
+
+                <div className="grid grid-cols-4 gap-8">
+                    <div>
+                        <div className="h-48 bg-red-500 rounded-lg overflow-hidden mb-6">
+                            <img src={BookOne} alt="book-1" />
+                        </div>
+                        <div className="mb-4">
+                            <h5 className="font-medium text-gray-800">Why reading is important for our children?</h5>
+                            <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore... <br /> <span className="text-purple-500 hover:text-purple-600">Continue Reading</span> </p>
+                        </div>
+                        <div className="flex items-center space-x-4">
+                            <div className="h-10 w-10 bg-purple-500 rounded-full"></div>
+                            <div>
+                                <b className="text-gray-800">Haerul wardy</b>
+                                <p className="text-gray-500">2 days ago</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="h-48 bg-red-500 rounded-lg overflow-hidden mb-6">
+                            <img src={BookOne} alt="book-1" />
+                        </div>
+                        <div className="mb-4">
+                            <h5 className="font-medium text-gray-800">Why reading is important for our children?</h5>
+                            <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore... <br /> <span className="text-purple-500 hover:text-purple-600">Continue Reading</span> </p>
+                        </div>
+                        <div className="flex items-center space-x-4">
+                            <div className="h-10 w-10 bg-purple-500 rounded-full"></div>
+                            <div>
+                                <b className="text-gray-800">Haerul wardy</b>
+                                <p className="text-gray-500">2 days ago</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="h-48 bg-red-500 rounded-lg overflow-hidden mb-6">
+                            <img src={BookOne} alt="book-1" />
+                        </div>
+                        <div className="mb-4">
+                            <h5 className="font-medium text-gray-800">Why reading is important for our children?</h5>
+                            <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore... <br /> <span className="text-purple-500 hover:text-purple-600">Continue Reading</span> </p>
+                        </div>
+                        <div className="flex items-center space-x-4">
+                            <div className="h-10 w-10 bg-purple-500 rounded-full"></div>
+                            <div>
+                                <b className="text-gray-800">Haerul wardy</b>
+                                <p className="text-gray-500">2 days ago</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="h-48 bg-red-500 rounded-lg overflow-hidden mb-6">
+                            <img src={BookOne} alt="book-1" />
+                        </div>
+                        <div className="mb-4">
+                            <h5 className="font-medium text-gray-800">Why reading is important for our children?</h5>
+                            <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore... <br /> <span className="text-purple-500 hover:text-purple-600">Continue Reading</span> </p>
+                        </div>
+                        <div className="flex items-center space-x-4">
+                            <div className="h-10 w-10 bg-purple-500 rounded-full"></div>
+                            <div>
+                                <b className="text-gray-800">Haerul wardy</b>
+                                <p className="text-gray-500">2 days ago</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
