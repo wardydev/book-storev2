892a51cf3ec75b955e3c75a6080292b9d052ae16
wardy dev <wardydev@gmail.com>
2021-11-27 11:24:12 +0800

    repair book-collection

--- src/container/pages/Books/index.jsx
+++ src/container/pages/Books/index.jsx
@@ -44,18 +44,16 @@ export default function Books() {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
                             </svg>
                         </Link>
-                        <Link to="/books" className="flex
-                        
-                        
-                        
-                        
-                        
-                        items-center justify-between border border-gray-100 mt-1 rounded-lg px-6 py-4">
+                        <Link to="/books" className="flex items-center justify-between border border-gray-100 mt-1 rounded-lg px-6 py-4">
                             <span className="font-medium text-gray-800">Price Range</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-purple-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
                             </svg>
                         </Link>
+                        <div className="mt-6">
+                            <button className="w-full py-2 mb-4 bg-purple-600 hover:bg-purple-700 text-gray-50 rounded-lg">Refine Search</button>
+                            <button className="w-full py-2 border hover:bg-gray-100 border-gray-200 text-gray-500 rounded-lg">Reset Filter</button>
+                        </div>
                     </div>
                 </div>
                 <BookCollection />
